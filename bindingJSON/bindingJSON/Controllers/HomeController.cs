af50ed352a580f8568a19b60d448623e14b0dbd7
James <james@codeforcoffee.org>
2014-04-16 19:30:57 -0500

    sample project - init
    sample project for MVC Data Binding with JSON

---
+++ bindingJSON/bindingJSON/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bindingJSON.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+			// Initial View
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult PostSquirrel(Squirrel incomingSquirrel)
+        {
+			string status = null;
+			try { 
+				saveSquirrel(incomingSquirrel);
+				status = "If you don't see this, something went wrong.";
+			} catch (Exception e) {
+				// handle errors...;
+			}
+			return Json(status);
+        }
+
+		#region privateHelpers
+		private Boolean saveSquirrel(Squirrel incomingSquirrel) 
+		{
+			if (!incomingSquirrel.Age) {
+				// do something...
+				return false;
+			} else {
+				// do something positive!
+				return true;
+			}
+		}
+		#endregion
+    }
+}
---
+++ bindingJSON/bindingJSON/Models/Squirrel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace bindingJSON
+{
+	public class Squirrel
+	{
+		public string Name { get; set; }
+		public int? Age { get; set; }	// squirrels aren't required tell us their age
+		public int Acorns { get; set; }
+		public char Gender { get; set; }
+		public string Hobby { get; set; }
+	}
+}
+
